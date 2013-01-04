aa222890cacfdbcbab560ba3c877185f67ad8742
James McKinney <james@slashpoundbang.com>
2013-01-04 15:31:23 -0500

    add stubs for specs

---
+++ spec/token_action/orm/active_record_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+if DEVISE_ORM == :active_record
+  describe TokenAction::Token do
+  end
+end
---
+++ spec/token_action/orm/mongoid_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+if DEVISE_ORM == :mongoid
+  describe TokenAction::Token do
+  end
+end
