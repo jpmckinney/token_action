d4a07746207003425eed723b421889843354e92a
James McKinney <james@slashpoundbang.com>
2013-01-05 13:33:34 -0500

    add model spec

---
+++ spec/token_action/mixins/model_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+# @todo
+describe TokenAction::Token do
+  it {should validate_presence_of :kind}
+  it {should validate_uniqueness_of :token}
+
+  describe '#create' do
+    it 'should set the token' do
+      FactoryGirl.create(:token).token.should_not be_nil
+    end
+  end
+end
